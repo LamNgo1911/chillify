98539bc3f2a787c50f5da5e360320d40acfa4c4d
Diddayngo <lamngo606@gmail.com>
2023-04-08 13:44:36 +0700

    change site

--- server/app.js
+++ server/app.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 // middleware
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://bespoke-frangollo-b3a1d3.netlify.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
  }
  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://bespoke-frangollo-b3a1d3.netlify.app');
     res.header('Access-Control-Allow-Credentials', true);
     next();
   });
