a9d9839ff1ed129f2081d7e036643591716b9220
Diddayngo <lamngo606@gmail.com>
2023-08-09 15:57:46 +0300

    check axios

--- client/src/components/Songbar/Player.jsx
+++ client/src/components/Songbar/Player.jsx
@@ -30,8 +30,7 @@ function Player({activeSong, isPlaying, volume, seekTime, onEnded, onTimeUpdate,
 
   return (
     <audio
-    src={activeSong?.songUrl ? `${axios.defaults.baseURL}/api/v/chart/stream/${activeSong.songUrl}` : ''}
-
+    src={activeSong?.songUrl ? `${axios.defaults.baseURL}/api/v1/chart/stream/${activeSong.songUrl}` : ''}
       ref={ref}
       loop={repeat}
       onEnded={onEnded}
