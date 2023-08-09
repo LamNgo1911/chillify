10a1783ec5a5dcc480de19bd8801075f357b8440
Diddayngo <lamngo606@gmail.com>
2023-08-09 16:17:44 +0300

    fix axios

--- client/src/components/Songbar/Player.jsx
+++ client/src/components/Songbar/Player.jsx
@@ -29,7 +29,7 @@ function Player({activeSong, isPlaying, volume, seekTime, onEnded, onTimeUpdate,
 
   return (
     <audio
-    src={activeSong?.songUrl ? `http://localhost:5000/api/v1/chart/stream/${activeSong.songUrl}` : ''}
+    src={activeSong?.songUrl ? `https://chillify-5lg9.onrender.com/api/v1/chart/stream/${activeSong.songUrl}` : ''}
       ref={ref}
       loop={repeat}
       onEnded={onEnded}
