379c51ca175a147e0300297a6d4066b2cab4afca
Diddayngo <lamngo606@gmail.com>
2023-09-15 09:07:02 +0300

    update server

--- server/app.js
+++ server/app.js
@@ -6,21 +6,38 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 // middleware
-const corsOptions = {
-  origin: "https://bespoke-frangollo-b3a1d3.netlify.app",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://bespoke-frangollo-b3a1d3.netlify.app"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-app.use(cors(corsOptions)); // Use this after the variable declaration
+// const corsOptions = {
+//   origin: "https://bespoke-frangollo-b3a1d3.netlify.app",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     corsOptions.origin
+//   );
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
+const allowedOrigins = [
+  "https://bespoke-frangollo-b3a1d3.netlify.app",
+  "http://localhost:3000", // Change the port if your localhost runs on a different port
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Enable CORS credentials
+};
+
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 app.use(express.json());
 app.get("/", (req, res) => {
   res.sendStatus(200);
