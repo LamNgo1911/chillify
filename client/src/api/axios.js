20ce257b9ed1ba5a8ac17a0057001e4475a026b4
Diddayngo <lamngo606@gmail.com>
2023-09-15 09:24:22 +0300

    update url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+let url;
+
+if(process.env.NODE_ENV === "production"){
+  url = "https://chillify-5lg9.onrender.com/api/v1"
+} else {
+  // Use the local server URL when in development
+  url = "http://localhost:5000/api/v1"; // Change this to your local server's URL
+}
+
 export default axios.create({
-  baseURL: "https://chillify-5lg9.onrender.com/api/v1",
+  baseURL: url,
 });
