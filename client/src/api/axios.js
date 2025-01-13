687ecdae0aedf1af9c1f363f31ad16d7400eb124
Diddayngo <lamngo606@gmail.com>
2025-01-13 13:11:47 +0200

    update the port from backend with AWS host

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 let url;
 
-if(process.env.NODE_ENV === "production"){
-  url = "https://chillify-5lg9.onrender.com/api/v1"
+if (process.env.NODE_ENV === "production") {
+  url = "https://oinvb9t9nh.execute-api.us-east-1.amazonaws.com/api/v1";
 } else {
   // Use the local server URL when in development
   url = "http://localhost:5000/api/v1"; // Change this to your local server's URL
@@ -13,4 +13,4 @@ export default axios.create({
   baseURL: url,
 });
 
-export {url}
+export { url };
