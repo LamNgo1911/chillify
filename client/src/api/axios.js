036dd4c61fb29df5398c834aed62e9971a584e5b
Diddayngo <lamngo606@gmail.com>
2025-01-13 14:06:35 +0200

    change the backend url with AWS host

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let url;
 
 if (process.env.NODE_ENV === "production") {
-  url = "https://chillify-5lg9.onrender.com/api/v1";
+  url = "https://oinvb9t9nh.execute-api.us-east-1.amazonaws.com/api/v1";
 } else {
   // Use the local server URL when in development
   url = "http://localhost:5000/api/v1"; // Change this to your local server's URL
